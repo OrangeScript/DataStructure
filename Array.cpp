d4c724cfdafa1c1bf6921476b4e9865a1650226b
cxsy <cxsy@qq.com>
2025-10-12 14:07:23 +0800

    array_structure

---
+++ Array.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+class Array{
+public:
+    Array(int size = 10): mCur(0), m_capacity(size){
+        m_data = new int[m_capacity];
+    }
+    ~Array()
+    {
+        delete[] m_data;
+        m_data = nullptr; 
+    }
+private:
+    int* m_data;
+    int mCur;
+    int m_capacity;
+    void expand(int newSize)
+    {
+        int *p = new int[newSize];
+        memcpy(p,m_data,sizeof(int) * m_capacity);
+        delete []m_data;
+        m_data = p;
+        m_capacity = newSize;
+    }
+public:
+    void push_back(int value)
+    {
+        if(mCur == m_capacity)
+        {
+            expand(2 * m_capacity);
+        }
+        m_data[mCur++] = value;
+    }
+    void pop_back()
+    {
+        if(mCur == 0)
+            return;
+        mCur--;
+    }
+
+    void insert(int pos, int val)
+    {
+        if(pos < 0 || pos >mCur)
+        {
+            return;
+        }
+
+        if(mCur == m_capacity)
+        {
+            expand(2 * m_capacity);
+        }
+
+        for(int i = mCur - 1; i >= pos; i--)
+        {
+            m_data[i+1] = m_data[i];
+        }
+
+        m_data[pos] = val;
+        mCur++;
+    }
+
+    void erase(int pos)
+    {
+        if(pos >= mCur || pos < 0)
+        {
+            return;
+        }
+
+        for(int i = pos; i < mCur - 1; i++)
+        {
+            m_data[i] = m_data[i+1];
+        }
+
+        mCur--;
+    }
+
+    int find(int val)
+    {
+        for(int i = 0;i < mCur; i++)
+        {
+            if(m_data[i] == val)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    void show()const
+    {
+        for(int i = 0;i < mCur; i++)
+        {
+            std::cout << m_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main()
+{
+    Array arr;
+    srand(time(0));
+    for(int i = 0; i< 10; i++)
+    {
+        arr.push_back(rand()%100);
+    }
+    arr.show();
+    arr.pop_back();
+    arr.show();
+    arr.insert(0,10);
+    arr.show();
+    arr.insert(10,200);
+    arr.show();
+}
