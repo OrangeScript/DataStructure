268105cc9fabcf3f643f91aa4f64353272936f9a
cxsy <cxsy@qq.com>
2025-10-18 15:42:36 +0800

    circle link2

--- CircleList.cpp
+++ CircleList.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 
 class CircleLink
 {
@@ -40,6 +43,81 @@ public:
         Node* n = new Node(val);
         tail_->next_ = n;
         n->next_ = head_;
+        tail_ = n;
     }
 
+    void InsertHead(int val)
+    {
+        Node* n = new Node(val);
+        if(head_->next_ == nullptr)
+        {
+            tail_ = n;
+        }
+        n->next_ = head_->next_;
+        head_->next_ = n;
+    }
+
+    void Remove(int val)
+    {
+        Node* q = head_;
+        Node* p = head_->next_;
+
+        while(p != head_)
+        {
+            if(p->data_ == val)
+            {
+                q->next_ = p->next_;
+                delete p;
+                if(q->next_ == head_)
+                {
+                    tail_ = q;
+                }
+                return;
+            }
+            else
+            {
+                q = p;
+                p = p->next_;
+            }
+        }
+    }
+
+    bool Find(int val)const
+    {
+        Node* p = head_->next_;
+        while(p != head_)
+        {
+            if(p->data_ == val)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void Show()const
+    {
+        Node* p = head_->next_;
+        while(p != head_)
+        {
+            std::cout << p->data_ << " ";
+            p = p->next_;
+        }
+        std::cout << std::endl;
+    }
 };
+
+int main()
+{
+    CircleLink* c = new CircleLink();
+    srand(time(NULL));
+    for(int i = 0; i < 10; i++)
+    {
+        c->InsertTail(rand()% 100);
+    }
+    c->Show();
+    c->InsertHead(100);
+    c->InsertTail(200);
+    c->Remove(200);
+    c->Show();
+}
