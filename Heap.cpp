8e328e3cfe50b45efc3145bff1e52941c8b976eb
cxsy <cxsy@qq.com>
2025-11-01 16:43:43 +0800

    Heapsort

--- Heap.cpp
+++ Heap.cpp
@@ -47,6 +47,10 @@ public:
         SiftDown(0,que_[0]);
     }
 
+    int top()
+    {
+        return que_[0];
+    }
     void Print()
     {
         for(int i = 0; i < size_ ; i ++)
@@ -55,7 +59,8 @@ public:
         }
         std::cout << std::endl;
     }
-
+    
+    friend void HeapSort(int arr[],int size);
 private:
     void SiftUp(int loc, int val)
     {
@@ -80,44 +85,111 @@ private:
 
     void SiftDown(int loc, int val)
     {
-        int child;
-        while((child = loc * 2 + 1) < size_)
+        int child = loc * 2 + 1;
+        if(child < size_)
         {
-            if(child + 1 < size_ && comp_(que_[child + 1], que_[child]))
+            if(child + 1 < size_ && comp_(que_[child + 1],que_[child]))
             {
-                child ++;
+                child = child + 1;
             }
+            if(comp_(que_[child],val))
+            {
+                que_[loc] = que_[child];
+                loc = child;
+                SiftDown(loc,val);
 
-            if (!comp_(que_[child], val))
-            break;
-
-            que_[loc] = que_[child];
-            loc = child;
+            }
+            else
+            {
+                que_[loc] = val;
+            }
         }
-        que_[loc] = val;
+        else
+        {
+            que_[loc] = val;
+        }
+        
     }
 };
 
+void siftdown(int arr[], int loc, int size)
+{
+    int child;
+    int val = arr[loc];
+    while((child = loc * 2 + 1) < size)
+    {
+        if(child + 1 < size && arr[child + 1] > arr[child])
+            child = child + 1;
+        if(arr[child] > val)
+        {
+            arr[loc] = arr[child];
+            loc = child;
+        }
+        else
+        {
+            break;
+        }
+    }
+    arr[loc] = val;
+    
+}
+
+void swap1(int arr[], int i, int j)
+{
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
+void HeapSort(int arr[], int size)
+{
+    
+    for(int i = (size - 1 - 1) / 2 ; i >= 0; i --)
+    {
+        siftdown(arr, i, size);
+    }
+    int index = size - 1;
+    for(int i = 0; i < size; i++)
+    {
+        swap1(arr,0,index);
+        siftdown(arr,0,index);
+        index --;
+    }
+}
+
+
+void HeapSort_rubbish(int arr[],int size)
+{
+    PriorityQueue q = PriorityQueue([](int a,int b){return a<b;});
+    for(int i = 0; i < size; i++)
+    {
+        q.push(arr[i]);
+    }
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = q.top();
+        q.pop();
+    }
+}
+#include <time.h>
+#include <stdlib.h>
 int main()
 {
-    PriorityQueue q;
-    q.push(5);
-    q.push(2);
-    q.push(3);
-    q.push(1);
-    q.push(4);
-    q.push(9);
-    q.push(10);
-    q.push(22);
-    q.push(4);
-    q.push(7);
-    q.Print();
-    q.pop();
-    q.Print();
-    q.pop();
-    q.Print();
-    q.pop();
-    q.Print();
-    q.pop();
-    q.Print();
+    srand(time(NULL));
+    int arr[10];
+    for(int i = 0; i< 10; i++)
+    {
+        arr[i] = rand()%100;
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    HeapSort(arr,10);
+
+    for(int i = 0; i< 10; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    std::cout << std::endl;
 }
--- Sort.cpp
+++ Sort.cpp
@@ -127,6 +127,10 @@ void InsertSort(int arr[], int size);
 void ShellSort(int arr[], int size);
 void SelectionSort(int arr[], int size);
 void BubbleSort(int arr[], int length);
+void QuickSort(int arr[],int left, int right);
+void MergeSort(int arr[], int left, int right);
+
+void RadixSort(int arr[], int size);
 #if 0
 int main()
 {
@@ -144,7 +148,8 @@ int main()
 }
 #endif
 
-#define COUNT 100000
+#define COUNT 30
+#if 0
 int main()
 {
     int arr[COUNT],brr[COUNT],crr[COUNT],drr[COUNT];
@@ -161,7 +166,7 @@ int main()
 
     clock_t begin,end;
     begin = clock();
-    BubbleSort(arr,COUNT);
+    QuickSort(arr,0,COUNT);
     end = clock();
     std::cout << "Bubble Sort spend:" << (end - begin) * 1.0 /CLOCKS_PER_SEC 
     << "s" << std::endl;
@@ -187,6 +192,27 @@ int main()
     std::cout << "Shell Sort spend:" << (end - begin) * 1.0 /CLOCKS_PER_SEC 
     << "s" << std::endl;
 }
+#endif
+//#if 0
+int main()
+{
+    
+    int arr[COUNT];
+    srand(time(NULL));
+
+    for(int i = 0; i < COUNT; i++)
+    {
+        int val = rand() ;
+        arr[i] = val;
+    }
+    arr[10] = -23;
+    arr[20] = -123;
+    arr[2] = -45;
+    Print(arr,COUNT);
+    RadixSort(arr,COUNT);
+    Print(arr,COUNT);
+}
+//#endif
 
 void InsertSort(int arr[], int size) {
     for(int i = 1; i < size; i++) {
@@ -201,6 +227,96 @@ void InsertSort(int arr[], int size) {
     }
 }
 
+void Merge(int arr[],int left,int mid,int right)
+{
+    int p = left;
+    int q = mid + 1;
+    int i = 0;
+    int *brr = new int[right - left + 1];
+    while(p <= mid && q <= right)
+    {
+        if(arr[p] <= arr[q])
+        {
+            brr[i++] = arr[p];
+            p ++; 
+        }
+        else
+        {
+            brr[i++] = arr[q];
+            q ++;
+        }
+    }
+    while(p <= mid)
+    {
+        brr[i++] = arr[p++];
+    }
+    while(q <= right)
+    {
+        brr[i++] = arr[q++];
+    }
+    //memcpy(arr+left,brr,i * sizeof(int));
+    for(int j = 0; j < i; j++)
+    {
+        arr[left + j] = brr[j];
+    }
+    delete [] brr;
+}
+void MergeSort(int arr[], int left, int right)
+{
+    if(left >= right)
+        return;
+    int mid = (left + right) / 2;
+    
+    MergeSort(arr, left, mid);
+    MergeSort(arr,mid + 1, right);
+    //[left,mid]
+    //[mid + 1, right]
+    Merge(arr,left,mid,right);
+}
+int Partition(int arr[], int left, int right);
+int Partition_Hoare(int a[], int left, int right);
+void swap1(int arr[], int l, int r);
+void QuickSort(int arr[],int left, int right)
+{
+    if(left >= right)
+        return;
+
+    int pivot = Partition(arr,left,right);
+    QuickSort(arr,left,pivot - 1);
+    QuickSort(arr,pivot + 1,right);
+}
+
+int Partition(int arr[], int left, int right)
+{
+    int val = arr[left];
+    int p = left, q = right;
+    while(q != p)
+    {
+        while(arr[q] >= val && q > p) q--;
+        while(arr[p] <= val && q > p) p++;
+        swap1(arr,p,q);
+    }
+    arr[left] = arr[p];
+    arr[p] = val;
+
+    return p;
+}
+int Partition_Hoare(int a[], int left, int right){
+    int i = left;
+    int j = right;
+    int key = a[left];
+
+    while(i != j){
+        while(i < j && a[j] >= key)      	 //向左找到小于基准值的值的下标
+            j--;
+        while(i < j && a[i] <= key)      	 //向右找到大于基准值的值的下标
+            i++;
+	swap1(a,i,j);
+	}
+    /*   i等于j时跳出循环 当前基准值此时在下标为i的位置(合适的位置)   */
+    swap1(a,left, i);	                 //最左边的元素变为处于当前合适位置的元素,把基准值放在合适位置                                                 
+    return i;                                  //返回合适位置(i,j都可以)
+}
 void swap1(int arr[], int l, int r)
 {
     int tmp = arr[l];
@@ -240,5 +356,43 @@ void ShellSort(int arr[], int size)
                 }
                 arr[k + gap] = val; 
             }
+    }
+}
+
+#include <vector>
+#include <string>
+using namespace std;
+void RadixSort(int arr[], int size)
+{
+    int maxData = abs(arr[0]);
+    for(int i = 1; i < size; i++)
+    {
+        if(abs(arr[i]) > maxData)
+        {
+            maxData = abs(arr[i]);
+        }
+    }
+     maxData = std::to_string(maxData).size();
+    vector<vector<int>> bucket;
+
+    for(int i = 0, dev = 1, mod = 10; i< maxData; i++,dev *= 10,mod *= 10)
+    {
+        bucket.resize(20);
+
+        for(int j = 0; j < size; j++)
+        {
+            bucket[arr[j] % mod / dev + 10].push_back(arr[j]);
+        }
+        int k = 0;
+        for(vector<int> v : bucket)
+        {
+            for(int x : v)
+            {
+                arr[k] = x;
+                k++;
+            }
+            
+        }
+        bucket.clear();
     }
 }
